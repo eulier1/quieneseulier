e06262ad38f90c057d2bbaf9e0188914982507d4
eulier <eulier1@gmail.com>
2018-09-08 02:21:24 -0400

    Deploy proper redirect done

--- src/app/mobile/mobile-routing.module.ts
+++ src/app/mobile/mobile-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MobileComponent } from './mobile.component';
+import { AppComponent } from '../app.component';
 
 const routes: Routes = [
-  { path: '', component: MobileComponent }
+  { path: 'mobile', component: MobileComponent },
+  { path: 'mobile',
+    redirectTo: '/mobile',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
